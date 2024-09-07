b86b2c84c8f8c56196a1934ab62c007149153894
BabiRP <131627843+BabiRP@users.noreply.github.com>
2024-09-07 00:37:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,40 @@
+let dados =  [
+
+    
+    {
+
+        titulo  : "Rayssa Leal",
+
+        descricao : "Rayssa Leal, também conhecida carinhosamente como Fadinha,é uma das maiores revelações do skate mundial. Com apenas 16 anos, ela já coleciona títulos importantes e inspira milhões de pessoas ao redor do globo. ",
+
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal#:~:text=Jhulia%20Rayssa%20Mendes%20Leal%20(%20Imperatriz",
+        
+
+        tags: "skate fada sk8"
+    },
+
+    {
+        
+        titulo : "Rebeca Andrade",
+
+        descricao : "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegrancia e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+
+        tags: "medalha ouro ginastica"
+
+    },
+
+    {
+
+        titulo: "Flavia Saraiva",
+
+        descricao: "Flávia Lopes Saraiva é uma ginasta brasileira que conquistou o coração de muitos com sua habilidade, carisma e determinação. Nascida no Rio de Janeiro em 1999, ela iniciou sua trajetória na ginástica ainda criança e rapidamente se destacou no cenário nacional e internacional.",
+    
+        link: "https://pt.wikipedia.org/wiki/Fl%C3%A1via_Saraiva",
+
+        tags: "ginastica medalha olimpiada"
+    }
+
+];
+
